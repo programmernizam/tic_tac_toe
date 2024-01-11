19e870ffa618681024b0503f5c8205147a7bdf82
programmerNizam <mdnizam12109@gmail.com>
2024-01-12 02:20:23 +0600

    Add Interactivity

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,15 @@
-export default function Square({ value }) {
+import { useState } from "react";
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      onClick={handleClick}
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+    >
       {value}
     </button>
   );
